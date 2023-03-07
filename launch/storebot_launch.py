2cd5f05fb109180d71e9f9e076c81107b8f04795
Konstantin Petrykin <kpetrykin@gmail.com>
2023-03-07 18:38:57 +0000

    Remove unnecessary nodes from launch file

--- launch/storebot_launch.py
+++ launch/storebot_launch.py
@@ -37,7 +37,7 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         output='screen',
-        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_link_footprint'],
+        arguments=['0', '0', '-0.29', '0', '0', '0', 'base_link', 'base_link_footprint'],
     )
     
     robot_state_publisher_node = Node(
@@ -53,21 +53,6 @@ def generate_launch_description():
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
     
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-    
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
-
     return LaunchDescription([
         webots,
         storebot_driver,
@@ -83,10 +68,6 @@ def generate_launch_description():
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node
     ])
