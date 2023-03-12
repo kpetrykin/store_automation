aef32da9cb583c7f78c5f0d0e727ee76312307f7
Konstantin Petrykin <kpetrykin@gmail.com>
2023-03-12 10:47:06 +0000

    Adding 'use_sim_time=true' to all nodes

--- launch/storebot_launch.py
+++ launch/storebot_launch.py
@@ -29,7 +29,8 @@ def generate_launch_description():
         output='screen',
         additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix() + 'storebot'},
         parameters=[
-            {'robot_description': robot_description},
+            {'robot_description': robot_description,
+             'use_sim_time': True},
         ],
     )
     
@@ -37,37 +38,47 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         output='screen',
-        arguments=['0', '0', '-0.29', '0', '0', '0', 'base_link', 'base_link_footprint'],
+        arguments=['0', '0', '-0.29', '0', '0', '0', 'base_link', 'base_footprint'],
+        parameters=[{'use_sim_time': True}]
+    )
+    
+    map_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
+        parameters=[{'use_sim_time': True}]
     )
     
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
     
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        arguments=[default_model_path],
+        # parameters=[{'use_sim_time': True}]
     )
     
     return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
+                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
         webots,
         storebot_driver,
         footprint_publisher,
+        map_publisher,
+        joint_state_publisher_node,
+        robot_state_publisher_node,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=webots,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
             )
         ),
-        
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        joint_state_publisher_node,
-        robot_state_publisher_node,
     ])
