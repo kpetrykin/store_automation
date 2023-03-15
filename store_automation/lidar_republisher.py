122daf0cd6b2546c94502889afb4ad354527a61d
Konstantin Petrykin <kpetrykin@gmail.com>
2023-03-15 06:16:49 +0000

    Adding lidar_republisher to add timestamp to webot's lidar msg

---
+++ store_automation/lidar_republisher.py
@@ -0,0 +1,26 @@
+from rclpy.time import Time
+from sensor_msgs.msg import LaserScan
+
+
+class LidarRepublisher:
+    def __init__(self, robot, ros_node) -> None:
+        self._robot = robot
+        self._ros_node = ros_node
+        
+        self._laser_scan_publisher = self._ros_node.create_publisher(
+            LaserScan, '/scan', 1)
+        
+        self._ros_node.create_subscription(
+            LaserScan, '/storebot/lidar', self._laser_scan_callback, 1)
+    
+    def _laser_scan_callback(self, msg: LaserScan) -> None:
+        timestamp = Time(seconds=self._robot.getTime()).to_msg() #self._ros_node.get_clock().now().to_msg() 
+        
+        timed_msg = msg
+        timed_msg.header.stamp = timestamp
+        
+        self._laser_scan_publisher.publish(timed_msg)
+        
+        
+        
+        
